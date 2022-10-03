46a2dedc068dca7a975c37ba472f8f1bcf451fa2
+ <+>
2022-10-03 15:59:41 +0100

    commit tree e587c8e968a0216aed00017fc34bd5334f53b34d

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if (typeof(word) === "string") {
+    for (let i = 0; i < word.length; i ++) {
+      if (word[i] != word[word.length - (i+1)]) {
+        if (word[i] === word[word.length - (i+1)]) {
+          i ++;
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    }
+  }
 }
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+  d - Check if word is string
+  d - make word an array
+  d - For each letter in the word
+  d - let i = 0
+  check if word[i] === word[length of array - (i+1)]
+  i += 1
+  if word[i] = word[length of array - (i+1)]
+  return true
+  else return false
+
 */
 
 /*
